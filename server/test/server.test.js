b5e671492d08c8cc9287c662a0328526a70cdeb1
poonam patel <patel.poonam4@gmail.com>
2017-06-13 00:36:51 +0530

    added change for testing get request

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,12 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos =[{text:'hello world'},{text:'goodbye world'}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('Post /todos', () => {
@@ -22,7 +26,7 @@ describe('Post /todos', () => {
                 if (err) {
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -39,10 +43,21 @@ describe('Post /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e))
             })
     });
 
+});
+
+describe('GET /todos',()=>{
+    it('should get all the todoes in the db',(done)=>{
+request(app)
+.get('/todos')
+.expect(200)
+.expect((res)=>{
+    expect(res.body.todos.length).toBe(2);
+}).end(done);
+    });
 });
