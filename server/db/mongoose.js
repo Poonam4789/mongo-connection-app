6b3538aac3c4fbe65d17dc1f07d4f8cb6fa1caff
poonam patel <patel.poonam4@gmail.com>
2017-06-19 14:18:16 +0530

    made changes for deploy2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://poonam patel:poo040789nam@ds131312.mlab.com:31312/todoapp'||'mongodb://localhost:27017/TodoApp'); //mongodb://<dbuser>:<dbpassword>@ds131312.mlab.com:31312/todoapp
-:
+
 module.exports={
     mongoose
 };
