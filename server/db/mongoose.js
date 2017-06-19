9f4e734b034566e9438265ef1319115c41382276
poonam patel <patel.poonam4@gmail.com>
2017-06-19 18:46:54 +0530

    changes for deployment on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI||'mongodb://localhost:27017/TodoApp');
 //mongodb://poonam.patel:asdfg@1989@ds131312.mlab.com:31312/todoapp
+//mongodb://<dbuser>:<dbpassword>@ds131312.mlab.com:31312/todoapp
+
 module.exports={
     mongoose
 };
