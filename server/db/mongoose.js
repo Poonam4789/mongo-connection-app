54267bbcdabf6b4ade6f968e8070d79f13b28e5e
poonam patel <patel.poonam4@gmail.com>
2017-08-02 21:26:58 +0530

    mongoUri changed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
 //mongoose.connect(process.env.MONGOLAB_URI);
 //mongodb://poonam.patel:asdfg@1989@ds131312.mlab.com:31312/todoapp
 //mongodb://<dbuser>:<dbpassword>@ds131312.mlab.com:31312/todoapp
